e0df8f018580c55e693d50218ea298b8296bd77d
akstrov <yalam385.ya@gmail.com>
2023-09-16 15:50:19 +0100

    fix some bugs and change unit test

--- battery/spindlerBattery.py
+++ battery/spindlerBattery.py
@@ -5,7 +5,6 @@ from datetime import datetime
 class SpindlerBattery(Battery):
     def __init__(self, last_service_date):
         self.last_service_date = last_service_date
-        self.current_date
 
     def needs_service(self):
         self.current_date = datetime.today().date()
